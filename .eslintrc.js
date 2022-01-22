4fded9cb9ada9ed2f6edd8551cf389bae6f02636
Yu Yin <yxonic@gmail.com>
2022-01-22 23:27:31 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# typescript-template
+
+![Supported node versions](https://img.shields.io/node/v/husky)
+
+<img align="right" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png" height="120px" alt="typescript logo">
+
+A _minimal_ TypeScript project template for both frontend and backend development.
+
+### Features
+
+* Start a TypeScript project with **best practices**
+* Better **developer experience** with formatting, linting and VSCode extentions
+* A good starting point for both **frontend and backend** development
+
+### Usage
+#### Use Github template
+[Use this template](https://github.com/lem0nle/typescript-template/generate) to create your own repo on Github.
+
+#### Create locally
+Also, you can create a local project with this template.
+
+```bash
+npx degit lem0nle/typescript-template my-ts-project
+cd my-ts-project
+npm install
+```
